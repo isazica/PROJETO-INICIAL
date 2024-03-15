b577f480f4ffdfa2f80ae52ba7a03fdb4b7dcec3
isazica <isabella2709.marques@gmail.com>
2024-03-15 09:04:57 -0300

    variavel nome

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
 
 function alertar(){
-    alert("Voce clicou no botao!!!");
+    alert(nome.value + " " + "clicou no botao!!!");
 }
 
