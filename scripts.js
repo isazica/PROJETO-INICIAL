a64c018aa325ee8fc550807c475ae37b8c83494b
isazica <isabella2709.marques@gmail.com>
2024-03-18 09:56:12 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -2,7 +2,31 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
+var telefone = document.getElementById("Telefone");
+
+var CEP = documment.getElementById("CEP");
+
+var Logradouro = document.getElementById("Logradouro");
+
+var Numero = document.getElementById("Numero");
+
+var Complemento = document.getElementById("Complemento");
+
+var Bairro = document.getElementById("Bairro");
+
 function alertar(){
-    alert(nome.value + " " + "clicou no botao!!!");
+   //alert(nome.value + " " + "clicou no botao!!!");
+   saida.innerText = "Nome:" + " " + nome.value +
+   "\n E-mail:" + " " + email.value + 
+   "\n Telefone:" + " " + telefone.value + 
+   "\n CEP:" + " " + CEP.value +
+   "\n Logradouro:" + " " + Logradouro.value +
+   "\n Numero:" + " " + Numero.value +
+   "\n Complemento:" + " " + Complemento.value +
+   "\n Bairro:" + " " + Bairro.value;
 }
 
